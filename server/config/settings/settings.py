769127435cd2d95c0fbe149da1c2952dabf18f15
Alexander Shcherbanyuk <s0ul@protonmail.com>
2021-04-20 18:26:31 +0700

    Init Django

---
+++ server/config/settings/settings.py
@@ -0,0 +1,123 @@
+import os
+from pathlib import Path
+
+import dj_database_url
+
+
+BASE_DIR = Path(__file__).parents[2]
+SECRET_KEY = os.environ.get('SECRET_KEY', 'SECRET_KEY')
+ROOT_URLCONF = 'config.urls'
+WSGI_APPLICATION = 'config.wsgi.application'
+ALLOWED_HOSTS = ['*']
+SITE_ID = 1
+
+##################################################################
+# Debug settings (with docker)
+##################################################################
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+##################################################################
+# Databases settings (with docker)
+##################################################################
+
+DATABASES = {'default': dj_database_url.config()}
+
+##################################################################
+# Logging settings
+##################################################################
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+##################################################################
+# Templates, middleware settings
+##################################################################
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR.joinpath('templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+##################################################################
+# Password validation settings
+##################################################################
+
+if not DEBUG:
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+
+
+##################################################################
+# Static files settings (CSS, JavaScript, Images)
+##################################################################
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATICFILES_DIRS = ('static',)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+
+FILE_UPLOAD_PERMISSIONS = 0o777
+FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o777
